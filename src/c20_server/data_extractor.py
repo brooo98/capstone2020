3d162613c092f904d161eaa3cfebc6f36efc822a
Abrar Hilal <47187809+brooo98@users.noreply.github.com>
2020-04-23 11:33:25 +0200

    Saving data to disk

---
+++ src/c20_server/data_extractor.py
@@ -0,0 +1,40 @@
+from collections import namedtuple
+import os
+
+Data = namedtuple('Data', ['folder_name', 'file_name', 'contents'])
+
+
+class MissingDataException(Exception):
+    pass
+
+
+class DataExtractor:
+
+    @staticmethod
+    def extract(list_of_data_dicts):
+        ret = []
+
+        for data_item in list_of_data_dicts:
+            try:
+                ret.append(Data(folder_name=data_item['folder_name'],
+                                file_name=data_item['file_name'],
+                                contents=data_item['data']))
+            except TypeError:
+                raise MissingDataException()
+        return ret
+
+
+    @staticmethod
+    def save_files_disk(Data_list):
+
+        for data in Data_list:
+            
+            directory = data.folder_name
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            
+            detination = data.folder_name + '/' +data.file_name
+            with open(detination,'w') as f:
+                f.write(data.contents)
+
+
